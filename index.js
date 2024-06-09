85a7215a735496b8184e3c725d1db68b259325b9
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-09 15:56:30 -0300

    seleccion de modelos

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
 import { HfInference } from "@huggingface/inference";
 import dotenv from 'dotenv';
 import fs from 'fs';
+import inquirer from 'inquirer';
 import open from "open";
-import readline from "readline";
 
 dotenv.config();
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const models = [
+    {
+        model: "runwayml/stable-diffusion-v1-5",
+        title: "Diffusion V1.5",
+    },
+    {
+        model: "stabilityai/sdxl-turbo",
+        title: "SDXL Turbo",
+    },
+    {
+        model: "CompVis/stable-diffusion-v1-4",
+        title: "Diffusion V1.4",
+    },
+    {
+        model: "runwayml/stable-diffusion-inpainting",
+        title: "Diffusion Inpainting",
+    },
+    {
+        model: "stabilityai/stable-diffusion-2-1-base",
+        title: "Diffusion 2.1 Base",
+    },
+    {
+        model: "stabilityai/stable-diffusion-2-1",
+        title: "Diffusion 2.1",
+    },
+    {
+        model: "SG161222/Realistic_Vision_V4.0_noVAE",
+        title: "Realistic Vision V4.0",
+    },
+    {
+        model: "h94/IP-Adapter-FaceID",
+        title: "IP Adapter FaceID",
+    },
+    {
+        model: "ByteDance/SDXL-Lightning",
+        title: "SDXL Lightning",
+    }
+];
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "selectedModel",
+        message: "Select a model:",
+        choices: models.map(model => ({
+            name: model.title,
+            value: model.model,
+        })),
+    },
+    {
+        type: "input",
+        name: "prompt",
+        message: "Enter your prompt:",
+    },
+    {
+        type: "input",
+        name: "fileName",
+        message: "Enter the file name (without extension):",
+    }
+]).then(answers => {
+    const { selectedModel, prompt, fileName } = answers;
+    downloadImage(prompt, `${fileName}.jpeg`, selectedModel);
 });
 
-rl.question("-+ Enter your prompt\n-> ", (text) => {
-    rl.question("-+ Enter the file name (without extension):\n-> ", async (fileName) => {
-        await downloadImage(text, fileName + ".jpeg");
-        rl.close();
-    });
-});
-
-async function downloadImage(text, path) {
+async function downloadImage(text, path, model) {
     const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
-    const model = "runwayml/stable-diffusion-v1-5";
 
     try {
         const response = await hf.textToImage({
