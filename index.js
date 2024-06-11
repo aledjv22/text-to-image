a541ef968079eb117e1ada534afa41d6dcba4627
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-10 21:28:00 -0300

    Lista de modelos separado

--- index.js
+++ index.js
@@ -4,47 +4,9 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import inquirer from 'inquirer';
 import open from "open";
+import models from "./models.js";
 
 dotenv.config();
-
-const models = [
-    {
-        model: "runwayml/stable-diffusion-v1-5",
-        title: "Diffusion V1.5",
-    },
-    {
-        model: "stabilityai/sdxl-turbo",
-        title: "SDXL Turbo",
-    },
-    {
-        model: "CompVis/stable-diffusion-v1-4",
-        title: "Diffusion V1.4",
-    },
-    {
-        model: "runwayml/stable-diffusion-inpainting",
-        title: "Diffusion Inpainting",
-    },
-    {
-        model: "stabilityai/stable-diffusion-2-1-base",
-        title: "Diffusion 2.1 Base",
-    },
-    {
-        model: "stabilityai/stable-diffusion-2-1",
-        title: "Diffusion 2.1",
-    },
-    {
-        model: "SG161222/Realistic_Vision_V4.0_noVAE",
-        title: "Realistic Vision V4.0",
-    },
-    {
-        model: "h94/IP-Adapter-FaceID",
-        title: "IP Adapter FaceID",
-    },
-    {
-        model: "ByteDance/SDXL-Lightning",
-        title: "SDXL Lightning",
-    }
-];
 
 inquirer.prompt([
     {
---
+++ models.js
@@ -0,0 +1,40 @@
+const models = [
+    {
+        model: "runwayml/stable-diffusion-v1-5",
+        title: "Diffusion V1.5",
+    },
+    {
+        model: "stabilityai/sdxl-turbo",
+        title: "SDXL Turbo",
+    },
+    {
+        model: "CompVis/stable-diffusion-v1-4",
+        title: "Diffusion V1.4",
+    },
+    {
+        model: "runwayml/stable-diffusion-inpainting",
+        title: "Diffusion Inpainting",
+    },
+    {
+        model: "stabilityai/stable-diffusion-2-1-base",
+        title: "Diffusion 2.1 Base",
+    },
+    {
+        model: "stabilityai/stable-diffusion-2-1",
+        title: "Diffusion 2.1",
+    },
+    {
+        model: "SG161222/Realistic_Vision_V4.0_noVAE",
+        title: "Realistic Vision V4.0",
+    },
+    {
+        model: "h94/IP-Adapter-FaceID",
+        title: "IP Adapter FaceID",
+    },
+    {
+        model: "ByteDance/SDXL-Lightning",
+        title: "SDXL Lightning",
+    }
+];
+
+export default models;
